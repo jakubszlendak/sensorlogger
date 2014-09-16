21d50159e0f49e7a2e282d5da7a6e23bb642107d
jakubszlendak <szlendak.jakub@gmail.com>
2014-09-16 22:26:51 +0200

    Develops parser. Issue with float conversion

--- python/server.py
+++ python/server.py
@@ -3,13 +3,25 @@ from time import *
 
 def parseData(data,outerSplitStr=' | ',innerSplitStr=':'):
     lines=data.splitlines()
+    parsedData = []
     for line in lines:
+        dataVect = []
         splittedLine = line.split(outerSplitStr)
         print(splittedLine)
-        for element in splittedLine:
-            valueStr=element.split(innerSplitStr)
-            print(valueStr)
-
+        if (len(splittedLine)==3):
+            for element in splittedLine:
+                valueStr=element.split(innerSplitStr)
+                try:
+                    x=float(valueStr[-1])
+                    dataVect.append(x)
+                    
+                except ValueError:
+                    print('zesralo sie')
+                    pass
+                print(valueStr[-1])
+            parsedData.append(dataVect)
+    return parsedData
+    
 host = ''
 port = 8888
 backlog = 5
@@ -19,17 +31,12 @@ s.bind((host,port))
 s.listen(backlog)
 client,addres = s.accept()
 file = open("sensorlogger.txt", "w")
-
-
-
-
 try:
     while(True):
         data=client.recv(size)
         dataDecoded = data.decode()
         file.write(dataDecoded+'\n')
-        parseData(dataDecoded)
-        #values = dataDecoded.split(' | ')
+        print(parseData(dataDecoded))
         #print(values)
         sleep(0.03)
     client.close()
